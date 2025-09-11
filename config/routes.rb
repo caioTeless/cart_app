7060b1147d815607188b0fdccff38ab6eeec276a
cteles <caioo.teless@gmail.com>
2025-09-10 23:37:55 -0300

    feat: new table cart_item, crud for cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,13 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+
+  resource :cart, only: [:show, :create] do
+    patch :add_item
+    put   :add_item
+    delete ':product_id', action: :destroy_item, as: :destroy_item
+  end
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
